f229e251849e116444983712789e719191f8a843
Keith Alexander <keithalexander@keithalexander.co.uk>
2017-11-04 21:38:08 +0000

    added timestamp

--- Main.elm
+++ Main.elm
@@ -6,16 +6,31 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
+import Task exposing (..)
+import Time exposing (..)
+import Date
 
 
 
 main =
-  Html.beginnerProgram
-    { model = model
+  Html.program
+    { init = init
     , view = view
     , update = update
+    , subscriptions = subscriptions
     }
 
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- INIT
+init : (Model, Cmd Msg)
+init =
+  (model, Cmd.none)
 
 
 -- MODEL
@@ -30,7 +45,7 @@ type alias Model =
 type alias Ascent = 
   {
       grade: String
-    , dateTime: String
+    , dateTime: Time
   }
 
 model : Model
@@ -50,15 +65,18 @@ model =
 type Msg
     = Grade String
     | Add
+    | CurrentTime Time
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg ) 
 update msg model =
   case msg of
     Grade grade ->
-      { model | grade = grade }
+      ({ model | grade = grade }, Cmd.none)
     Add ->
-      { model | ascents = (Ascent model.grade "today")::model.ascents}
+      (model, Time.now |> Task.perform CurrentTime)
+    CurrentTime time ->
+      ({ model | ascents = (Ascent model.grade time)::model.ascents}, Cmd.none)
 
 
 -- VIEW
@@ -68,15 +86,19 @@ view : Model -> Html Msg
 view model =
  Grid.container []
         [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
+        , Grid.row [] [ Grid.col [] [ h1 [] [text "Indoor Climbing Log"]] ]
         , Grid.row []
             [ Grid.col []
                 [ 
                   select [ onInput Grade  ] (List.map (\x -> option [] [text x]) model.gradeList)
                 , input [ type_ "button", value "Add", onClick Add  ] []
-                , ul [] (List.map (\x -> li [] [text (x.grade ++ " " ++ x.dateTime)] ) model.ascents)
+                , ul [] (List.map (\x -> li [] [text (x.grade ++ " " ++ (viewTime x.dateTime))] ) model.ascents)
                 ]
             ]
 
         ]   
-
-
+viewTime : Time -> String
+viewTime time = 
+     time
+        |> Date.fromTime 
+        |> toString
