38f81ca5d184ce95e296e71a371b0527bdfc14e9
Keith Alexander <keithalexander@keithalexander.co.uk>
2017-11-05 03:03:33 +0000

    added delete buttons

--- Main.elm
+++ Main.elm
@@ -1,61 +1,92 @@
 -- Read all about this program in the official Elm guide:
 -- https://guide.elm-lang.org/architecture/user_input/forms.html
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+
+module Main exposing (..)
+
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
+import Date
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 import Task exposing (..)
 import Time exposing (..)
-import Date
-
 
 
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
+
 
 -- INIT
-init : (Model, Cmd Msg)
+
+
+init : ( Model, Cmd Msg )
 init =
-  (model, Cmd.none)
+    ( model, Cmd.none )
+
 
 
 -- MODEL
 
 
 type alias Model =
-  { grade : String
-  , ascents : List Ascent
-  , gradeList: List String
-  }
+    { grade : String
+    , ascents : List Ascent
+    , gradeList : List String
+    }
 
-type alias Ascent = 
-  {
-      grade: String
-    , dateTime: Time
-  }
+
+type alias Ascent =
+    { grade : String
+    , dateTime : Time
+    }
+
 
 model : Model
 model =
-  Model "" [] ["4", "4+"
-  , "5", "5+"
-  , "6a", "6a+", "6b", "6b+", "6c", "6c+"
-  , "7a", "7a+", "7b", "7b", "7b+", "7c", "7c+"
-  , "8a", "8a+", "8b", "8b", "8b+", "8c", "8c+"
-  ]
+    Model ""
+        []
+        [ "4"
+        , "4+"
+        , "5"
+        , "5+"
+        , "6a"
+        , "6a+"
+        , "6b"
+        , "6b+"
+        , "6c"
+        , "6c+"
+        , "7a"
+        , "7a+"
+        , "7b"
+        , "7b"
+        , "7b+"
+        , "7c"
+        , "7c+"
+        , "8a"
+        , "8a+"
+        , "8b"
+        , "8b"
+        , "8b+"
+        , "8c"
+        , "8c+"
+        ]
 
 
 
@@ -65,40 +96,54 @@ model =
 type Msg
     = Grade String
     | Add
+    | Del Ascent
     | CurrentTime Time
 
 
-update : Msg -> Model -> ( Model, Cmd Msg ) 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Grade grade ->
-      ({ model | grade = grade }, Cmd.none)
-    Add ->
-      (model, Time.now |> Task.perform CurrentTime)
-    CurrentTime time ->
-      ({ model | ascents = (Ascent model.grade time)::model.ascents}, Cmd.none)
+    case msg of
+        Grade grade ->
+            ( { model | grade = grade }, Cmd.none )
 
+        Add ->
+            ( model, Time.now |> Task.perform CurrentTime )
+        Del ascent ->
+            ({model| ascents = List.filter (notAscent ascent) model.ascents}, Cmd.none)
+
+        CurrentTime time ->
+            ( { model | ascents = Ascent model.grade time :: model.ascents }, Cmd.none )
+
+
+notAscent : Ascent -> Ascent -> Bool
+notAscent a b =
+    a /= b
 
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
- Grid.container []
+    Grid.container []
         [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
-        , Grid.row [] [ Grid.col [] [ h1 [] [text "Indoor Climbing Log"]] ]
+        , Grid.row [] [ Grid.col [] [ h1 [] [ text "Indoor Climbing Log" ] ] ]
         , Grid.row []
             [ Grid.col []
-                [ 
-                  select [ onInput Grade  ] (List.map (\x -> option [] [text x]) model.gradeList)
-                , input [ type_ "button", value "Add", onClick Add  ] []
-                , ul [] (List.map (\x -> li [] [text (x.grade ++ " " ++ (viewTime x.dateTime))] ) model.ascents)
+                [ select [ onInput Grade ] (List.map (\x -> option [] [ text x ]) model.gradeList)
+                , input [ type_ "button", value "Add", onClick Add ] []
+                , ul [] 
+                    (List.map (\x -> li [] [ 
+                        text (x.grade ++ " " ++ viewTime x.dateTime)
+                       , input [type_ "button", value "X", onClick (Del x) ] []
+                    ]) 
+                    model.ascents)
                 ]
             ]
+        ]
 
-        ]   
+
 viewTime : Time -> String
-viewTime time = 
-     time
-        |> Date.fromTime 
+viewTime time =
+    time
+        |> Date.fromTime
         |> toString
